0499fd65b309449fa96dcd73c28882c4600d2f35
Its-Am1r <am1rrrzare85@gmail.com>
2023-08-12 11:51:01 +0330

    S1 first challenge

---
+++ script.js
@@ -0,0 +1,34 @@
+// let js = "amazing";
+// if (js === "amazing") alert("javascript is fun");
+
+// 4 - +8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = "JM";
+// let PI = 3.14;
+
+// let myFirstJob = "programmer";
+// let myCurrentJob = "teacher";
+
+// console.log(myFirstJob);
+
+let john_weight = 85;
+let john_height = 1.76;
+
+let mark_weight = 95;
+let mark_height = 1.88;
+
+let john_BMI = john_weight / john_height ** 2;
+let mark_BMI = mark_weight / mark_height ** 2;
+const markHigherBMI = mark_BMI > john_BMI;
+
+console.log(mark_BMI, john_BMI, markHigherBMI);
