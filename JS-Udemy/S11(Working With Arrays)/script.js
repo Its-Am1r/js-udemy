3dfca10d9eddb3e785a3d0b88b9cb3368e40b2f8
Its-Am1r <am1rrrzare85@gmail.com>
2023-08-21 17:47:34 +0330

    s11 first changes

--- JS-Udemy/S11(Working With Arrays)/script.js
+++ JS-Udemy/S11(Working With Arrays)/script.js
@@ -61,16 +61,191 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTMLm = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawl';
+
+    const html = `   <div class="movements__row">
+    <div class="movements__type movements__type--deposit">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+  </div>`;
+  });
+};
+displayMovements(account1.movements);
+
+const calcdisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcdisplayBalance(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLocaleLowerCase()
+      .split(' ')
+      .map((name) => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+////////////////////////
+// //array mathods
+
+// //SLICE
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// console.log([...arr]);
+
+// //SPLICE
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// arr.splice(1, 2);
+// console.log(arr);
+
+// //REVERSE
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
+
+// //CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+
+// //JOIN
+// console.log(letters.join(' - '));
+// ////////////////////////////
+
+///////////////////////////
+//for of loop in array
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: you deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: you withdraw ${Math.abs(movement)}`);
+//   }
+// }
+
+// //for each loop in array
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: you deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: you withdraw ${Math.abs(mov)}`);
+//   }
+// });
+
+// //0:function(200)
+// //1:function(450)
+// //2:function(400)
+//////////////////////////
+
+/////////////////////////
+//for each loop in map and set
+
+// //map
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// //set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, _, map) {
+//   console.log(`${value}: ${value}`);
+// });
+////////////////////////
+
+/////////////////////
+//challenge 1
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice();
+//   dogsJuliaCorrected.splice(0, 1);
+//   dogsJuliaCorrected.splice(-2);
+
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   console.log(dogs);
+
+//   dogs.forEach(function (dog, i) {
+//     if (dog >= 3) {
+//       console.log(`dog number ${i + 1} is an adult and is ${dog} years old`);
+//     } else {
+//       console.log(`dog number ${i + 1} is still poppy and is ${dog} years old`);
+//     }
+//   });
+// };
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+///////////////////////
+
+//////////////////////
+//filter method
+// const deposites = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+// console.log(movements);
+// console.log(deposites);
+
+// const depositesFor = [];
+// for (const mov of movements) if (mov > 0) depositesFor.push(mov);
+// console.log(depositesFor);
+
+// const withdrawls = movements.filter(function (mov) {
+//   return mov < 0;
+// });
+
+// const withdrawlsFor = [...withdrawls];
+// console.log(withdrawlsFor);
+///////////////////////
+
+//////////////////////
+//reduce method
+
+//regular function
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`iteraation ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+
+// //arrow function
+// const balance = movements.reduce((acc, cur) => acc + cur, 0);
+// console.log(balance);
+
+// //with for loop
+// let balance2 = 0;
+// for (const mov of movements) balance2 += mov;
+// console.log(balance2);
+
+// //maximum value
+// const max = movements.reduce((acc, mov) => {
+//   if (acc > mov) return acc;
+//   else return mov;
+// }, movements[0]);
+// console.log(max);
+
+/////////////////////////
