17c7ada58f60141396af69b68da116e97f74e69a
ItsAm1r <am1rrrzare85@gmail.com>
2023-08-28 16:30:32 +0330

    final change

--- JS-Udemy/S13(Advanced Dom)/script.js
+++ JS-Udemy/S13(Advanced Dom)/script.js
@@ -162,7 +162,7 @@ const allSections = document.querySelectorAll('.section');
 const revealSection = function (entries, observer) {
   const [entry] = entries;
 
-  // if (!entry.isIntersecting) return;
+  if (!entry.isIntersecting) return;
 
   entry.target.classList.remove('section--hidden');
   observer.unobserve(entry.target);
