4d5ccdc1ed6cfe715108bc6ff80f2d7763be952e
ItsAm1r <am1rrrzare85@gmail.com>
2023-09-07 15:51:29 +0330

    async changes

--- JS-Udemy/async.js
+++ JS-Udemy/async.js
@@ -1,56 +1,91 @@
-// function sayHello() {
+// function sayHello(data) {
 //   return new Promise((resolve, reject) => {
-//     resolve('hello');
+//     if (typeof data === 'string') {
+//       resolve(data);
+//     } else {
+//       reject(data);
+//     }
 //   });
 // }
 
-// sayHello()
+// sayHello('Ali')
 //   .then((res1) => res1.split(''))
 //   .then((res2) => res2.reverse())
 //   .then((res3) => res3.join(''))
-//   .then((final) => console.log(final));
+//   .then((final) => console.log(final))
+//   .catch(() => {
+//     console.log('Wrtie a string');
+//   });
 
 //     ////////////////////////////////////////////
 
 // const myPromise = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     const randomNumber = Math.random();
-//     console.log(randomNumber);
+
 //     if (randomNumber < 0.5) {
-//       resolve();
+//       resolve(randomNumber);
 //     } else {
-//       reject();
+//       reject(randomNumber);
 //     }
 //   }, 2000);
 // });
 
 // myPromise
-//   .then(() => {
-//     console.log('its resolved');
+//   .then((randomNumber) => {
+//     console.log('its resolved', randomNumber);
 //   })
-//   .catch(() => {
-//     console.log('its rejected');
+//   .catch((randomNumber) => {
+//     console.log('its rejected', randomNumber);
 //   });
 
-/////////////////////////////////////
+//////////////////////////////////////
 
-// async function getData() {
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const randomNumber = Math.random();
+//     if (randomNumber < 0.5) {
+//       resolve(randomNumber);
+//     } else {
+//       reject(randomNumber);
+//     }
+//   }, 2000);
+// });
+
+// async function consumePromise() {
 //   try {
-//     const data = await new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         const randomNumber = Math.random();
-//         console.log(randomNumber);
-//         if (randomNumber < 0.5) {
-//           resolve('Data retrieved successfully!');
-//         } else {
-//           reject();
-//         }
-//       }, 2000);
-//     });
-//     console.log(data);
+//     const result = await myPromise;
+//     console.log('Promise resolved', result);
 //   } catch {
-//     console.log('There is something wrong with your code');
+//     console.log('Promise rejected', result);
 //   }
 // }
 
-// getData();
+// consumePromise();
+
+///////////////////////////////////
+// async function DataJohn() {
+//   try {
+//     function fetchData() {
+//       return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//           const data = { username: 'John Doe', age: 25 };
+//           if (data.username === 'John Doe' && data.age === 25) {
+//             resolve(data);
+//           } else {
+//             reject();
+//           }
+//         }, 2000);
+//       });
+//     }
+
+//     const result = await fetchData();
+//     console.log('Data:', result);
+//   } catch {
+//     console.log('Its Not The Data you wanted');
+//   }
+// }
+
+// DataJohn();
+
+///////////////////////////////////
