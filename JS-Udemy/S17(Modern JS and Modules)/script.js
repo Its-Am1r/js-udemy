ae48316685dad25a3d8e887fa114a50a5482a9fc
ItsAm1r <am1rrrzare85@gmail.com>
2023-09-09 09:04:29 +0330

    s17

---
+++ JS-Udemy/S17(Modern JS and Modules)/script.js
@@ -0,0 +1,5 @@
+//IMPORTING MODULES
+import { addToCart } from './shoppingCart';
+console.log('IMPORTING MODULES');
+
+addToCart('bread', 5);
---
+++ JS-Udemy/S17(Modern JS and Modules)/shoppingCart.js
@@ -0,0 +1,10 @@
+//EXPORTING MODULE
+console.log('EXPORTING MODULE');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
