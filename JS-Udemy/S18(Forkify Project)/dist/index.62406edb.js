8fbbf2ef3e872e1f26b396e840eab863f01c89a3
ItsAm1r <am1rrrzare85@gmail.com>
2023-09-19 13:52:40 +0330

    some changes ]

--- JS-Udemy/S18(Forkify Project)/dist/index.62406edb.js
+++ JS-Udemy/S18(Forkify Project)/dist/index.62406edb.js
@@ -5,131 +5,46 @@ const timeout = function (s) {
       reject(new Error(`Request took too long! Timeout after ${s} second`));
     }, s * 1000);
   });
-}; // https://forkify-api.herokuapp.com/v2
+};
+// https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
+const cards = document.querySelectorAll('.card');
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle('show', entry.isIntersecting);
+      if (entry.isIntersecting) observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
+const lastCardObserver = new IntersectionObserver(
+  (entries) => {
+    const lastCard = entries[0];
+    if (!lastCard.isIntersecting) return;
+    loadNewCard();
+    lastCardObserver.unobserve(lastCard.target);
+    lastCard.observe(document.querySelector('.card:last-child'));
+  },
+  {
+    rootMargin: '100px',
+  }
+);
+lastCardObserver.observe(document.querySelector('.card:last-child'));
+cards.forEach((card) => {
+  observer.observe(card);
+});
+const cardContainer = document.querySelector('.card-container');
+function loadNewCard() {
+  for (let i = 0; i < 10; i++) {
+    const card = document.createElement('div');
+    card.textContent = 'New Card';
+    card.classList.add('card');
+    observer.observe(card);
+    cardContainer.append(card);
+  }
+}
 
 //# sourceMappingURL=index.62406edb.js.map
-
-const showRecipe = async function () {
-  try {
-    //LOADING RECIPE
-    const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=e468b5d1-67be-4318-90de-c8911b2a6f5f'
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    console.log(res, data);
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(rec);
-    //RENDERING RECIPE
-    const markup = ` <figure class="recipe__fig">
-    <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
-    <h1 class="recipe__title">
-      <span>${recipe.title}</span>
-    </h1>
-  </figure>
-
-  <div class="recipe__details">
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="src/img/icons.svg#icon-clock"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
-      <span class="recipe__info-text">minutes</span>
-    </div>
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="src/img/icons.svg#icon-users"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
-      <span class="recipe__info-text">servings</span>
-
-      <div class="recipe__info-buttons">
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="src/img/icons.svg#icon-minus-circle"></use>
-          </svg>
-        </button>
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="src/img/icons.svg#icon-plus-circle"></use>
-          </svg>
-        </button>
-      </div>
-    </div>
-
-    <div class="recipe__user-generated">
-      <svg>
-        <use href="src/img/icons.svg#icon-user"></use>
-      </svg>
-    </div>
-    <button class="btn--round">
-      <svg class="">
-        <use href="src/img/icons.svg#icon-bookmark-fill"></use>
-      </svg>
-    </button>
-  </div>
-
-  <div class="recipe__ingredients">
-    <h2 class="heading--2">Recipe ingredients</h2>
-    <ul class="recipe__ingredient-list">
-      <li class="recipe__ingredient">
-        <svg class="recipe__icon">
-          <use href="src/img/icons.svg#icon-check"></use>
-        </svg>
-        <div class="recipe__quantity">1000</div>
-        <div class="recipe__description">
-          <span class="recipe__unit">g</span>
-          pasta
-        </div>
-      </li>
-
-      <li class="recipe__ingredient">
-        <svg class="recipe__icon">
-          <use href="src/img/icons.svg#icon-check"></use>
-        </svg>
-        <div class="recipe__quantity">0.5</div>
-        <div class="recipe__description">
-          <span class="recipe__unit">cup</span>
-          ricotta cheese
-        </div>
-      </li>
-    </ul>
-  </div>
-
-  <div class="recipe__directions">
-    <h2 class="heading--2">How to cook it</h2>
-    <p class="recipe__directions-text">
-      This recipe was carefully designed and tested by
-      <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
-      directions at their website.
-    </p>
-    <a
-      class="btn--small recipe__btn"
-      href="${recipe.sourceUrl}"
-      target="_blank"
-    >
-      <span>Directions</span>
-      <svg class="search__icon">
-        <use href="src/img/icons.svg#icon-arrow-right"></use>
-      </svg>
-    </a>
-  </div>`;
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
-  } catch (err) {
-    alert(err);
-  }
-};
-showRecipe();
